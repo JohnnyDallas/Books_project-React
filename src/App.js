71bb8eb382f374e63496d4ecbe98894c1eeaa286
Johnny <vfscorbandallas@gmail.com>
2023-02-11 16:12:35 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { useEffect, useContext } from 'react';
+import BookCreate from './components/BookCreate';
+import BookList from './components/BookList';
+import BooksContext from './context/books';
+
+function App() {  
+   const { fetchBooks } = useContext(BooksContext);
+
+   useEffect(() => {
+      fetchBooks();
+   }, [fetchBooks]);
+
+   // DONT DO THIS:
+   // fetchBooks();
+
+   
+
+   return (
+    <div className='app'>
+      <h1>Reading List</h1>
+        <BookList /> 
+        <BookCreate />
+    </div>
+   ); 
+}
+
+export default App;
